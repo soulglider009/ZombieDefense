80e924c672d5ac97cb9c5d3f6b0377ac58aaf980
jsharp <mrjasharp@gmail.com>
2016-11-04 14:18:39 +0800

    Initial commit

---
+++ Source/ZombieDefense/ZombieDefenseGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ZombieDefenseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBIEDEFENSE_API AZombieDefenseGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
